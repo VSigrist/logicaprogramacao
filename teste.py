44f36ce6b79d6a0660495b7f25e3f3314e9723d7
Vitor Sigrist <81169652+VSigrist@users.noreply.github.com>
2021-05-21 16:06:46 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 # cria e imprime matriz
+
 def cria_matriz(linhas, colunas):
     matriz = []
     for i in range(linhas):
@@ -20,7 +21,8 @@ def imprime_matriz(matriz):
 matriz = cria_matriz(5, 4)
 imprime_matriz(matriz)
 
-#le precos e imprime nota fiscal
+# le precos e imprime nota fiscal
+
 with open('precos.txt') as arquivo:
     try:
         linhas = arquivo.readlines()
@@ -39,6 +41,7 @@ with open('./nota_fiscal.txt', 'w') as nota_fiscal:
     nota_fiscal.close()
     
 # notafiscal_2
+
     produtos = []
 precos = []
 with open('compras.txt', 'r') as compras:
@@ -61,5 +64,46 @@ with open('./nota_fiscal_plus.txt', 'w') as nota_fiscal:
         linhas_da_nota.append(f'total \t {sum(precos)}')
         nota_fiscal.write(str.join('\n', linhas_da_nota))
     nota_fiscal.close()
+
+# mostra o menor de 3 numeros
+
+def minimo_de_3(num1, num2, num3):
+    if num1 < num2 and num1 < num3:
+        return num1
+    elif num2 < num3:
+        return num2
+    return num3
+
+print(minimo_de_3(9, 8, 10))
+
+# retorna o minimo
+
+def meu_minimo (lista):
+  minimo = lista[0]
+  for elemento in lista:
+    if elemento < minimo:
+      minimo = elemento 
+  return minimo
+
+# cria funcao de soma
+
+def minha_soma(lista):
+  soma=0
+  for elemento in lista:
+    soma+=elemento
+  return soma
+
+# ordenando maior para o menor
+
+
+def meu_sort(lista):
+  tam=len(lista)
+  for i in range(tam):
+    for j in range(i + 1, tam):
+      if lista[i] > lista[j]:
+        temp = lista[i]
+        lista[i] = lista[j]
+        lista[j] = temp
+  return lista
 
 #
